3189749a63c4cb16cf1f0545651d1985415e3573
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2025-11-02 01:01:41 +0100

    feat: make the background black to make it more mysterious

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ type Game struct {
 }
 
 func NewGame(app *app.Application, scene *core.Node, cam *camera.Camera, log *slog.Logger) *Game {
-	app.Gls().ClearColor(.5, .5, .5, 1)
+	app.Gls().ClearColor(.0, .0, .0, 1)
 
 	onResize := func(evname string, ev any) {
 		w, h := app.GetSize()
