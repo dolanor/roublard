90e8a0c481155f429122cc05fe3aed51e35bd94e
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2026-08-05 04:25:29 +0200

    refactor: remove internal min/max function and use the now builtin ones

--- level.go
+++ level.go
@@ -203,19 +203,3 @@ func (level *Level) IsOpaque(x, y int) bool {
 	idx := level.GetIndexFromXY(x, y)
 	return level.Tiles[idx].TileType == TileTypeWall
 }
-
-// Max returns the larger of x or y.
-func max(x, y int) int {
-	if x < y {
-		return y
-	}
-	return x
-}
-
-// Min returns the smaller of x or y.
-func min(x, y int) int {
-	if x > y {
-		return y
-	}
-	return x
-}
